ed9da6c6c8dc39f0a9ca6510cfc8c8856f3b638b
chrisl <chrisl@pageuppeople.com>
2016-04-28 15:04:19 +1000

    Started fleshing out the Games CRUD page- you can now save entries and they will appear in the index list!
    Needs more work though.

--- cruddy-games/games-crud.js
+++ cruddy-games/games-crud.js
@@ -1,20 +1,58 @@
 angular.module('games-crud', ['ngRoute'])
+    .service('Model', function()
+    {
+        var self = this;
+        var data = [];
+        self.get = function()
+        {
+            return data;
+        };
+    })
     .config(function($routeProvider)
     {
+        var resolver = 
+        {
+            model: function(Model)
+            {
+                return Model.get();
+            }
+        };
+        
         $routeProvider
             .when('/',
             {
                 controller:'ListController as list',
                 templateUrl:'list.html',
+                resolve:resolver
             })
             .when('/new',
             {
-                controller:'ListController as list',
+                controller:'EditController as editor',
                 templateUrl:'edit.html',
+                resolve:resolver
             })
             .otherwise({redirectTo:'/'});
     })
-    .controller('ListController', function()
+    .controller('ListController', function(model)
     {
+        var self = this;
         
+        self.model = model;
+    })
+    .controller('EditController', function($location, model)
+    {
+        var self = this;
+        
+        self.game = 
+        {
+            name: 'Rocket League',
+            genre: 'Racing',
+            score: 100
+        };
+        
+        self.save = function()
+        {
+            model.push(self.game);
+            $location.path('/');
+        };
     });
