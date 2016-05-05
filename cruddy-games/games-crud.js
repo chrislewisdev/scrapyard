f16476d5b1ea9933ee1feaf9528176fa87ef7333
chrisl <chrisl@pageuppeople.com>
2016-05-05 14:42:47 +1000

    Games CRUD: - Added edit/delete from list page - Show all game info in list page - Simple validation for edit page

--- cruddy-games/games-crud.js
+++ cruddy-games/games-crud.js
@@ -1,13 +1,32 @@
 angular.module('games-crud', ['ngRoute'])
+    //Define the Model service, which will contain our data that persists across controllers.
     .service('Model', function()
     {
         var self = this;
-        var data = [];
+        var data = 
+        [
+            {
+                name:'Halo',
+                genre:'FPS',
+                score:85
+            },
+            {
+                name:'Rocket League',
+                genre:'Racing',
+                score:77
+            },
+            {
+                name:'Hitman',
+                genre:'Action',
+                score:80
+            }
+        ];
         self.get = function()
         {
             return data;
         };
     })
+    //Set up our routing for the index and edit pages.
     .config(function($routeProvider)
     {
         var resolver = 
@@ -31,28 +50,68 @@ angular.module('games-crud', ['ngRoute'])
                 templateUrl:'edit.html',
                 resolve:resolver
             })
+            .when('/edit/:gameId',
+            {
+                controller:'EditController as editor',
+                templateUrl:'edit.html',
+                resolve:resolver
+            })
             .otherwise({redirectTo:'/'});
     })
+    //The ListController, used for the index page
     .controller('ListController', function(model)
     {
         var self = this;
         
         self.model = model;
+        
+        self.delete = function(index)
+        {
+            model.splice(index, 1);
+        }
     })
-    .controller('EditController', function($location, model)
+    //The EditController, used when editing or creating new objects
+    .controller('EditController', function($location, $routeParams, model)
     {
         var self = this;
         
-        self.game = 
+        self.gameId = $routeParams.gameId;
+        self.isNew = (self.gameId === undefined);
+        
+        //New item? Initialise a fresh object.
+        if (self.isNew)
         {
-            name: 'Rocket League',
-            genre: 'Racing',
-            score: 100
-        };
+            self.game = 
+            {
+                name: '',
+                genre: '',
+                score: 100
+            };
+        }
+        //Otherwise, create a copy of our target object to edit.
+        else
+        {
+            self.target = model[self.gameId];
+            self.game = 
+            {
+                name: self.target.name,
+                genre: self.target.genre,
+                score: self.target.score
+            }
+        }
+        
         
         self.save = function()
         {
-            model.push(self.game);
+            //If it's new, append it to our model. If it's not, replace the old object.
+            if (self.isNew)
+            {
+                model.push(self.game);
+            }
+            else
+            {
+                model[self.gameId] = self.game;
+            }
             $location.path('/');
         };
     });
