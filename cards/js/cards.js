4444a7825490ef4364cc39f1ffdfeedc7ada1f98
Chris Lewis <cl_myles@hotmail.com>
2016-05-26 23:32:32 +1000

    Card Browser: - Moved the card loading logic into a service - All cards are now loaded up-front, providing faster class-switch feedback - Added some page limiting to the browser (can't move pages yet though) - Added neutral cards!

--- cards/js/cards.js
+++ cards/js/cards.js
@@ -1,40 +1,101 @@
 angular.module('cards', [])
-    //TODO: Define a service that holds all cards and provides access to them based on filtering
-    .controller('TempController', function($http)
+    .service('Collection', function($http, $q)
     {
         var self = this;
         
+        self.requestHeaders = {"X-Mashape-Key":"VcJN1iKXCVmshlD4bo5VlLG5so94p1gFd6ojsnRNhvlTDetFLL"};
+        
         //Storage for all currently displayed cards
-        self.model = [];
+        self.cards = [];
         //Storage for all valid classes
         self.classes = [];
-        //Our currently selected class
-        self.currentClass = null;
-        //Is true while we are doing any initial setup, false after
-        self.isInitialising = true;
+        //True once we've loaded up our base information
+        self.isInitialised = false;
+        
+        self.deferredLoading = $q.defer();
         
         //Initialise our list of classes
         $http.get('https://omgvamp-hearthstone-v1.p.mashape.com/info',
-        { headers: {"X-Mashape-Key":"VcJN1iKXCVmshlD4bo5VlLG5so94p1gFd6ojsnRNhvlTDetFLL"}})
+        { headers: self.requestHeaders } )
         .then(function success(response)
         {
+            //TODO: Any validation?
+            
             self.classes = response.data.classes;
-            self.currentClass = self.classes[0];
-            self.isInitialising = false;
-            self.load();
+            
+            //The 10th class is Dream, stupidly, while Neutral is not a class- so just shove it in there
+            self.classes[self.classes.length - 1] = 'Neutral';
+            
+            self.deferredLoading.resolve();
+            
+            //Retrieve cards separately for each class (so we get results back sooner)
+            self.classes.forEach(function(cardClass)
+            {
+                $http.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards/classes/' + cardClass + '?collectible=1',
+                { headers: self.requestHeaders } )
+                .then(function success(response)
+                {
+                    //TODO: Any validation?
+                    response.data.forEach(function(card)
+                    {
+                        //Neutral cards have no playerClass property, so set it explicitly for consistency
+                        card.playerClass = cardClass;
+                        self.cards.push(card);
+                    });
+                },
+                function error(response)
+                {
+                    console.log('Unable to retrieve cards for ' + cardClass);
+                });
+            });
         },
         function error(response)
         {
             console.log('Unable to initialise :(');
         });
+    })
+    .controller('BrowserController', function($http, Collection)
+    {
+        var self = this;
         
-        this.setClass = function(toClass)
+        self.collection = Collection;
+        
+        //Our currently selected class
+        self.currentClass = null;
+        
+        //Wait for the collection to initialise if necessary before we do.
+        if (!self.collection.isInitialised)
         {
-            self.currentClass = toClass;
-            self.load();
+            self.collection.deferredLoading.promise.then(function()
+            {
+                self.initialise();
+            });
+        }
+        else
+        {
+            self.initialise();
         }
         
-        this.getButtonClass = function(ofClass)
+        /**
+         * Initialises our current class selection to the first available class.
+         */
+        self.initialise = function()
+        {
+            self.currentClass = self.collection.classes[0];
+        }
+        
+        /**
+         * Sets the currently selected class to the given class.
+         */
+        self.setClass = function(toClass)
+        {
+            self.currentClass = toClass;
+        }
+        
+        /**
+         * Returns the appropriate CSS class for a class-button if it's selected; blank otherwise.
+         */
+        self.getButtonClass = function(ofClass)
         {
             if (ofClass == self.currentClass)
             {
@@ -44,21 +105,5 @@ angular.module('cards', [])
             {
                 return '';
             }
-        }
-        
-        this.load = function()
-        {
-            $http.get('https://omgvamp-hearthstone-v1.p.mashape.com/cards/classes/' + self.currentClass + '?collectible=1', 
-            {headers: {"X-Mashape-Key":"VcJN1iKXCVmshlD4bo5VlLG5so94p1gFd6ojsnRNhvlTDetFLL"}})
-            .then(function success(response)
-            {
-                // console.log(response);
-                self.model = response.data;
-                console.log('Loaded up the JSON! You should see it now...');
-            },
-            function error(response)
-            {
-                console.log('Unable to contact Hearthstone JSON!');
-            });
         }
     });
